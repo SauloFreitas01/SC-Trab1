8c250e8b4e69f19367e4b29365c3ca1fdb2791ab
Saulo Freitas <69553799+SauloFreitas01@users.noreply.github.com>
2023-10-02 01:52:32 -0300

    Update trabalho1.py
    adicionando segunda parte do trabalho

--- trabalho1.py
+++ trabalho1.py
@@ -34,20 +34,40 @@ def decipher(cypher_msg, key):
     decipher_msg = ''.join([chr(((ord(char.upper()) - ord(key.upper()) + 26) % 26) + ord('A')) if char.isalpha() else char for char, key in zip(cypher_msg, keystream)])
     return decipher_msg
 
-def break_cipher(cipher_msg, language):
+language_freq = {
+    'en': {'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253, 'e': 12.702, 'f': 2.228, 'g': 2.015, 'h': 6.094,
+           'i': 6.966, 'j': 0.153, 'k': 0.772, 'l': 4.025, 'm': 2.406, 'n': 6.749, 'o': 7.507, 'p': 1.929,
+           'q': 0.095, 'r': 5.987, 's': 6.327, 't': 9.056, 'u': 2.758, 'v': 0.978, 'w': 2.360, 'x': 0.150,
+           'y': 1.974, 'z': 0.074},
+    'pt': {'a': 14.63, 'b': 1.04, 'c': 3.88, 'd': 4.99, 'e': 12.57, 'f': 1.02, 'g': 1.30, 'h': 1.28,
+           'i': 6.18, 'j': 0.40, 'k': 0.02, 'l': 2.78, 'm': 4.74, 'n': 5.05, 'o': 10.73, 'p': 2.52,
+           'q': 1.20, 'r': 6.53, 's': 7.81, 't': 4.34, 'u': 4.63, 'v': 1.67, 'w': 0.01, 'x': 0.47,
+           'y': 0.01, 'z': 0.47}
+}
+
+
+def get_most_common_letter(language):
+    if language == 'en':
+        freq_table = language_freq['en']
+    elif language == 'pt':
+        freq_table = language_freq['pt']
+    else:
+        raise ValueError("Idioma não suportado.")
+
+    most_common_letter = max(freq_table, key=freq_table.get)
+    return most_common_letter
+
+def break_cipher(cipher_msg, language='en'):
     if language not in ['en', 'pt']:
         print("Idioma não suportado.")
         return
 
-    if language == 'en':
-        alphabet = string.ascii_uppercase
-        most_common_letter = 'E'
-    else:
-        alphabet = string.ascii_uppercase + 'Ç'
-        most_common_letter = 'A'
+    most_common_letter = get_most_common_letter(language)
+    alphabet = string.ascii_uppercase
 
     message = unidecode(cipher_msg)
 
+    # Determinar o tamanho da chave
     key_lengths = []
     for length in range(1, len(cipher_msg)):
         substrings = [message[i::length] for i in range(length)]
@@ -64,6 +84,7 @@ def break_cipher(cipher_msg, language):
         print("Não foi possível determinar o tamanho da chave.")
         return
 
+    # Quebrar a cifra para cada tamanho da chave possível
     for key_length in key_lengths:
         print(f"\nTentando quebrar a cifra com chave de tamanho {key_length}:")
         possible_keys = []
@@ -106,4 +127,5 @@ ciphered_msg = input("Insira a mensagem para quebra :")
 language = input("Idioma (en/pt): ")
 
 break_cipher(ciphered_msg, language)
+
 
